d0945bb2bc1c9bb5390de93742176a61793ac0c3
jam3oun00 <mjamoun28@gmail.com>
2023-10-16 21:40:03 +0100

    add required dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  components: [
+    { path: '~/components/common' },
+    { path: '~/components/hallows' },
+    { path: '~/components/utils' },
+  ],
+  devtools: { enabled: false },
 })
