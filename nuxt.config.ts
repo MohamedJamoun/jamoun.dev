63e44815745c916e0d6c7481378553ef547263cc
jam3oun00 <mjamoun28@gmail.com>
2023-10-17 20:50:55 +0100

    add base fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,23 @@
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  app: {
+    head:{
+      link: [
+        { 
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500;600;700&family=Young+Serif&display=swap',
+        },
+      ],
+    },
+  },
   components: [
     { path: '~/components/common' },
     { path: '~/components/hallows' },
     { path: '~/components/utils' },
   ],
-  css: ['~/styles/utils/tailwind.css'],
+  css: ['~/styles/utils/tailwind.css', '~/styles/main.scss'],
   postcss: {
     plugins: {
       tailwindcss: {},
